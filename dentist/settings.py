6fcea13e8ee8f5eac32d7ddfd003beeec8eafe61
Dee Jill <daajill71@gmail.com>
2023-05-20 11:01:08 +0300

    first commit

--- dentist/settings.py
+++ dentist/settings.py
@@ -126,8 +126,14 @@ STATIC_URL = 'static/'
     #os.path.join(BASE_DIR, 'static'),
 #] changed it to the static lines of code below for railway webhosting May 3 2023
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+
+    ]
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
